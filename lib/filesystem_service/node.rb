6ebaa004e0622d419a9ab9aa72fe59574d01e6c1
Jamie Paton <jamiep@activestate.com>
2013-10-15 18:14:22 -0700

    update require path for fence

--- lib/filesystem_service/node.rb
+++ lib/filesystem_service/node.rb
@@ -1,6 +1,6 @@
 # Copyright (c) 2009-2011 VMware, Inc.
 $:.unshift File.join(File.dirname(__FILE__), ".")
-$:.unshift File.join( ENV['HOME'], 'stackato', 'vcap', 'fence', 'fence-client', 'lib' )
+$:.unshift File.join( ENV['HOME'], 'stackato', 'code', 'fence', 'fence-client', 'lib' )
 
 require "base/node"
 require "uuidtools"
